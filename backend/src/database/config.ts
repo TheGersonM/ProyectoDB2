a359b6091de77f38097bbff7de935dceb7dfe2f3
CarlosArgueta <carguetar@unah.hn>
2024-12-05 11:02:23 -0600

    Revert "Resuelto conflicto en package-lock.json"
    This reverts commit 8fda2a5e207e92d8246bd0b19e6ea01df1b7bf7d.

--- backend/src/database/config.ts
+++ backend/src/database/config.ts
@@ -1,18 +1,14 @@
-import dotenv from 'dotenv';
-
-// Carga las variables desde el archivo .env
-dotenv.config();
 
 export const config = {
-    server: "DESKTOP-3RSI9MI\\SQLEXPRESS", 
-    database:  "CentroMedicoLaPaz",
-    user:  "sa",
+    server: "Gerson\\SQLEXPRESS",
+    database: "CentroMedicoLaPaz",
+    user: "sa",
     password: "12345678",
     options: {
         encrypt: false,
-        port: 1433, 
+        port:  1433,
         enableArithAbort: true,
         textsize: 1000000000,
         trustServerCertificate: true,
-    },
-};
+    }
+}
